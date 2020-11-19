48332647212e47734bcfa314054d684395d89f96
Karol Skolasiński <karolskolasinski@gmail.com>
2020-11-19 20:47:10 +0100

    set mongoDB connection

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+
+// settings
 app.set('view engine', 'pug');
+const log = process.env.DB_CONFIG_LOGIN;
+const pass = process.env.DB_CONFIG_PASSWORD;
+const db = process.env.DB_CONFIG_DATABASE;
+mongoose.connect(`mongodb+srv://${log}:${pass}@shortme-fun-database.qeghd.mongodb.net/${db}?retryWrites=true&w=majority`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
 
+// routes
 app.get('/', (req, res) => {
     res.render('index');
 });
 
 
-app.listen(process.env.PORT || 8080, function () {
-    console.log('http://localhost:8080')
-})
+app.post('/shortMe', (req, res) => {
+
+});
+
+
+// run
+app.listen(process.env.PORT || 8080, () => {
+    console.log('http://localhost:8080');
+});
