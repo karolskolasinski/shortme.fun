6e675bbad5502d294e130c7061449f9e294f208b
Karol Skolasiński <karolskolasinski@gmail.com>
2020-11-25 19:36:13 +0100

    routingErrorHandler

--- index.js
+++ index.js
@@ -14,12 +14,22 @@ const db = process.env.DB_CONFIG_DATABASE;
 mongoose.connect(`mongodb+srv://${log}:${pass}@shortme-fun-database.qeghd.mongodb.net/${db}?retryWrites=true&w=majority`, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+}).catch(error => {
+    console.error('Probably wrong credentials provided.\n', error);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use('*/css', express.static('public/css'));
 app.use('*/img', express.static('public/img'));
 app.use('*/js', express.static('public/js'));
+
+
+const routingErrorHandler = (err, res, code, msg) => {
+    console.error(msg, err);
+    res.status(500);
+    res.render('error', { error: msg });
+};
+
 
 // routes
 app.get('/', async (req, res) => {
@@ -30,56 +40,60 @@ app.get('/', async (req, res) => {
 
         res.render('index', { shortUrls: shortUrls });
     } catch (err) {
-        console.error('trying to find url in database failed', err);
+        console.error('An attempt to find the first 10 shortened urls in the database has failed.\n', err);
+        /*
+        * Render the index page regardless of exceptions thrown due to database problems.
+        * The index should be rendered without these records.
+        * */
         res.render('index');
     }
 });
 
 
 app.post('/shortMe', async (req, res) => {
-    let code = 404;
-    let error = 'short address not found in database';
+    let shortUrl;
 
     try {
-        let shortUrl = await databaseModel.findOne({ full: req.body.fullUrl });
+        shortUrl = await databaseModel.findOne({ full: req.body.fullUrl });
+    } catch (err) {
+        routingErrorHandler(err, res, 404, 'An attempt to find a short address in the database has failed.\n');
+    }
 
+    try {
         if (!shortUrl) {
             shortUrl = req.body.premium ?
                 await databaseModel.create({ full: req.body.fullUrl, short: req.body.premium }) :
                 await databaseModel.create({ full: req.body.fullUrl });
-            code = 500;
-            error = 'unable to persist resource to database';
         }
 
         res.render('index', { full: shortUrl.full, short: shortUrl.short });
     } catch (err) {
-        console.log(err);
-        res.status(code);
-        res.render('error', { error: error });
+        routingErrorHandler(err, res, 500, 'An attempt to create the resource to the database has failed.\n');
     }
 });
 
 
 app.get('/:shortReq', async (req, res) => {
-    let code = 404;
-    let error = 'short address not found in database';
+    let shortUrl;
 
     try {
-        const foundShortUrl = await databaseModel.findOne({ short: req.params.shortReq });
+        shortUrl = await databaseModel.findOne({ short: req.params.shortReq });
+    } catch (err) {
+        routingErrorHandler(err, res, 404, 'An attempt to find a short address in the database has failed.\n');
+    }
 
-        if (!foundShortUrl) {
+    try {
+        if (!shortUrl) {
             res.status(404);
             return res.render('error');
         }
 
-        foundShortUrl.clicks++;
-        foundShortUrl.save();
+        await shortUrl.clicks++;
+        await shortUrl.save();
 
-        res.redirect(foundShortUrl.full);
+        res.redirect(shortUrl.full);
     } catch (err) {
-        console.log(err);
-        res.status(code);
-        res.render('error', { error: error });
+        routingErrorHandler(err, res, 500, 'An attempt to save the resource to the database has failed.\n');
     }
 });
 
